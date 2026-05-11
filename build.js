49be350d8e3a77cb53244171a25a29aba4ce8f15
FeasyPlan <lluispereira17@gmail.com>
2026-05-11 19:22:49 +0200

    Update build.js

--- build.js
+++ build.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 
 // Crear directorio dist
 fs.mkdirSync('./dist', { recursive: true });
@@ -9,8 +8,12 @@ let html = fs.readFileSync('./index.html', 'utf8');
 html = html.replace('{{SUPABASE_KEY}}', process.env.SUPABASE_KEY || '');
 fs.writeFileSync('./dist/index.html', html);
 
-// Copiar páginas legales
-fs.copyFileSync('./terminos.html', './dist/terminos.html');
-fs.copyFileSync('./privacidad.html', './dist/privacidad.html');
+// Copiar páginas legales y de error
+['terminos.html', 'privacidad.html', 'cookies.html', '404.html'].forEach(file => {
+  if (fs.existsSync(file)) {
+    fs.copyFileSync(file, './dist/' + file);
+    console.log('Copiado:', file);
+  }
+});
 
 console.log('Build completo ✓');
